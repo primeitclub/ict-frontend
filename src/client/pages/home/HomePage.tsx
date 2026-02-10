c7bdcc85f7f7b6595a6f6b683b6723f0d7e9cb86
derikesh <rikeshsherpa1@gmail.com>
2026-02-10 22:39:43 +0545

    small fix

--- src/client/pages/home/HomePage.tsx
+++ src/client/pages/home/HomePage.tsx
@@ -1,7 +1,7 @@
 import AboutSection from "./sections/about-section/AboutSection";
 import GallerySection from "./sections/gallery-section/GallerySection";
 import { LandingSection } from "./sections/landing-section/LandingSection";
-import MetricSection from "./sections/metric-section/MetricSection";
+import MetricSection from "./sections/metrics-section/MetricSection";
 
 // Home page main component
 export default function HomePage() {
