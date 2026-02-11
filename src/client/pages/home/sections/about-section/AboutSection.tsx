de45b43691a3a21ece1359ce8254f07ca090179d
Samyak Manandhar <samyak11manandhar@gmail.com>
2026-02-11 08:50:02 +0545

    fix: Update aboutsection

--- src/client/pages/home/sections/about-section/AboutSection.tsx
+++ src/client/pages/home/sections/about-section/AboutSection.tsx
@@ -1,19 +1,18 @@
-import AboutLogo from "../../../../../assets/logo.png";
+import AboutLogo from "../../../../../assets/logo.svg";
 import glow from "../../../../../assets/glow.png";
+import fb from "../../../../../assets/fb.png";
+import insta from "../../../../../assets/insta.png";
+import Eclipse from "../../../../../assets/ellipse.png";
 
 export const AboutSection = () => {
   return (
-    <div
-      className="relative overflow-hidden"
-      style={{
-        backgroundColor: "#020919",
-      }}
-    >
-      {/* Glow layer */}
-      <img src={glow} alt="glow" className="w-full absolute z-0" />
-\
+    <>
+    {/* Glow layer */}
+      <img src={glow} alt="glow" className="w-full absolute" />
+    <section>
+      
       {/* Content layer */}
-      <div className="relative z-10 flex flex-col md:flex-row items-center gap-8 md:gap-14 md:mx-12 lg:gap-16 xl:gap-20 my-[200px]">
+      <div className="mt-[500px] relative flex flex-col md:flex-row items-center gap-8 md:gap-14 md:mx-12 lg:gap-16 xl:gap-20">
         {/* Mobile Title */}
         <h2 className="block md:hidden text-white font-bold text-[28px]">
           About Us
@@ -26,11 +25,12 @@ export const AboutSection = () => {
             src={AboutLogo}
             alt="About"
           />
+          <img src={Eclipse} alt="glow" className="absolute ml-10" />
         </div>
 
         {/* Content */}
         <div className="w-full md:w-1/2 text-center md:text-left">
-          <h2 className="hidden md:block text-white font-bold text-5xl mb-2 lg:text-3xl md:text-2xl xl:my-8 xl:text-4xl">
+          <h2 className="hidden md:block text-white font-bold text-5xl mb-2 lg:text-3xl md:text-2xl xl:my-8 xl:text-6xl">
             <span className="bg-gradient-to-b from-[#DBF5FF] to-[#007AFF] text-transparent bg-clip-text">
               About
             </span>
@@ -48,9 +48,27 @@ export const AboutSection = () => {
             skills in technology, design, and innovation while fostering
             teamwork, leadership, and real-world problem-solving.
           </p>
+
+          <div className="flex gap-4 mt-6">
+            <a href="https://www.facebook.com/primeitclub" target="_blank" rel="noopener noreferrer">
+              <img
+                src={fb}
+                alt="Facebook"
+                className="w-8 h-8 inline-block cursor-pointer"
+              />
+            </a>
+            <a href="https://www.instagram.com/primeitclub" target="_blank" rel="noopener noreferrer">
+            <img
+              src={insta}
+              alt="Instagram"
+              className="w-8 h-8 inline-block cursor-pointer"
+            />
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
+    </>
   );
 };
 export default AboutSection;
