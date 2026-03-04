9df8bc21963d96624696b78e4423d6e7c39624c3
=Manjil <aryalmanzeel1@gmail.com>
2026-03-04 20:11:10 +0545

    chore: landing section responsive

--- src/client/pages/home/sections/landing-section/LandingSection.tsx
+++ src/client/pages/home/sections/landing-section/LandingSection.tsx
@@ -9,11 +9,11 @@ export function LandingSection() {
         <div className="items-center flex justify-center text-center bg-red">
           <div className="flex items-center gap-2 border-1 bg-red p-2 rounded-full  bg-white/5 backdrop-blur-lg border border-white/10">
             <Button variant="filled" size="small" label="Event on" />
-            <div className="flex text-xs items-center">12-13 January, 2025 <ArrowRight size={20} /> </div>{" "}
+            <div className="flex text-xs items-center gap-1">12-13 January, 2025 <ArrowRight size={20} /> </div>{" "}
           </div>
         </div>
-        <div className="flex flex-col gap-2 w-[100%] px-[1%]  sm:px-0 sm:w-[80%] md:w-[75%] lg:w-[60%]  text-center mx-auto  ">
-          <div className="flex text-[34px] m-auto sm:text-[50px] sm:leading-[46px]  md:text-[64px] font-[600] leading-[37px] px-3 md:leading-[73px] bg-gradient-to-r from-[#DBF5FF]  to-[#51A7FF] bg-clip-text text-transparent -tracking-[2px] ">
+        <div className="flex flex-col gap-2 w-[100%] px-[1%]  sm:px-0 sm:w-[80%] md:w-[75%] lg:w-[70%] xl:w-[60%] 2xl:w-[52%] text-center mx-auto  ">
+          <div className="flex text-[34px] m-auto sm:text-[50px] sm:leading-[46px]  md:text-[64px] 2xl:text-[80px] font-[600] leading-[37px] px-3 md:leading-[73px] bg-gradient-to-r from-[#DBF5FF]  to-[#51A7FF] bg-clip-text text-transparent -tracking-[2px] ">
             Fusion Of Tech Talent & Creativity
           </div>
           <div className="flex text-[10px] sm:text-[16px]  px-5 sm:px-9 font-[400]  sm:leading-[26px] my-3 tracking-[0.3px]">
