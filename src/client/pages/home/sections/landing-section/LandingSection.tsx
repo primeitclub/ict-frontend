2b79288e9f1d5525f14c6081926087cbc61c48a0
=Manjil <aryalmanzeel1@gmail.com>
2026-03-06 08:27:39 +0545

    chore : fixed the event

--- src/client/pages/home/sections/landing-section/LandingSection.tsx
+++ src/client/pages/home/sections/landing-section/LandingSection.tsx
@@ -13,11 +13,11 @@ export function LandingSection() {
             <div className="flex text-[14px] items-center gap-2 leading-3 font-normal -tracking-[0.598px]">12-13 January, 2025 <ArrowRight size={20} /> </div>{" "}
           </div>
         </div>
-        <div className="flex flex-col gap-2 w-[100%] px-[1%]  sm:px-0 sm:w-[80%] md:w-[75%] lg:w-[70%] xl:w-[60%] 2xl:w-[52%] text-center mx-auto  ">
+        <div className="flex flex-col gap-2 w-[100%] px-[1%]  sm:px-0 sm:w-[80%] md:w-[75%] lg:w-[70%] xl:w-[60%] 2xl:w-[52%] text-center  items-center  ">
           <div className="flex text-[34px] m-auto sm:text-[50px] sm:leading-[46px]  md:text-[64px] 2xl:text-[80px] font-[600] leading-[37px] px-3 md:leading-[73px] bg-gradient-to-r from-[#DBF5FF]  to-[#3a9dff] bg-clip-text text-transparent -tracking-[2px] ">
             Fusion Of Tech Talent & Creativity
           </div>
-          <div className="flex text-[10px] sm:text-[16px]  px-5 sm:px-9 font-[400]  sm:leading-[26px] my-3 tracking-[0.3px]">
+          <div className="flex text-[10px] items-center sm:text-[16px]  px-5 sm:px-9 font-[400] text-center m-auto sm:leading-[26px] my-3 tracking-[0.3px]">
             ICT Meetup, designed to inspire collaboration, encourage learning
             beyond classrooms, and transform ideas into real-world impact.
           </div>
