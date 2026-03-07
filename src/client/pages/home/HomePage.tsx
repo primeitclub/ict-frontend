34a92ab206d9517702ecb3404928d0f268639410
Sajal Maharjan <Sajalmaharjan512@gmail.com>
2026-03-07 21:16:44 +0545

    fic: typo

--- src/client/pages/home/HomePage.tsx
+++ src/client/pages/home/HomePage.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
   return (
     <main>
       <LandingSection />
-      <AboutSection />s
+      <AboutSection />
       <HighlightSection />
       {/* <GallerySection /> */}
       <SpeakerSection />
