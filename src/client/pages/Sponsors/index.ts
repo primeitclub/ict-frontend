03d520d54960d5fa116662a532064f5284db516b
derikesh <rikeshsherpa1@gmail.com>
2026-03-15 15:17:02 +0545

    test

--- src/client/pages/Sponsors/index.ts
+++ src/client/pages/Sponsors/index.ts
@@ -1,1 +1,1 @@
-// export {default as sponsors} from './Sponsors'
+export {default as Sponsors} from './Sponsors'
--- src/client/pages/index.ts
+++ src/client/pages/index.ts
@@ -1,5 +1,5 @@
 export * from './home'
-export * from "./event";
+export * from "./event"
 export * from "./teams"
 export * from "./sponsors"
 
