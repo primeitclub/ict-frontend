bb1a85a53e9df2d6cbe09b604e70b50d4e74d818
derikesh <rikeshsherpa1@gmail.com>
2026-03-15 15:07:31 +0545

    chore:build fix

--- src/client/components/icon/eventIcon.tsx
+++ src/client/components/icon/eventIcon.tsx
@@ -1,6 +1,4 @@
-import * as React from "react";
-
-const EventIcon = (props) => (
+const EventIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
