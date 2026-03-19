6b910af292b9e813e54e24c8cb3f0b10eef4d674
Aman sapkota <amansapkota14@gmail.com>
2026-03-19 17:45:47 +0545

    Header & Footer Fixed

--- src/client/layouts/headers/Logo/Logo2.tsx
+++ src/client/layouts/headers/Logo/Logo2.tsx
@@ -3,7 +3,7 @@ import logo2 from "../../../../assets/logo@2x.png"
 export default function Logo2(){
     return(
         <>
-         <img src={logo2} alt="logo" className="object-contain  h-12"/>
+         <img src={logo2} alt="logo" className="object-contain h-12"/>
         </>
     );
 }
--- src/client/layouts/headers/Navbar.tsx
+++ src/client/layouts/headers/Navbar.tsx
@@ -14,34 +14,33 @@ const Navbar = () => {
     { path: "/events", label: "Events" },
     { path: "/teams", label: "Teams" },
     { path: "/sponsors", label: "Sponsors" },
+     { path: "/contact", label: "Contact Us" }
   ];
 
   const [toggle, setToggle] = useState(false);
 
   return (
     <SectionContainer
-      as="header"
-      className="flex justify-between items-center py-4"
-    >
+  as="header"
+  className="sticky top-0 z-50 h-[63px] flex items-center justify-between bg-[#020919] px-6 !py-0"
+>
       <div className="hover:cursor-pointer" onClick={() => navigate("/")}>
         <Logo2 />
       </div>
       <div className=" sm:hidden">
-        <button onClick={() => setToggle(!toggle)} className="bg-secondary p-1">
+        <button onClick={() => setToggle(!toggle)} className="p-1 bg-secondary">
           {toggle ? <X /> : <Menu />}
         </button>
         {toggle && (
           <div className="fixed inset-6 top-[10%] bottom-[50%] bg-white  rounded-xl py-6 px-10 z-20 ">
-            <div className="flex flex-col text-xl  items-center gap-10 font-medium">
+            <div className="flex flex-col items-center gap-10 text-xl font-medium">
               {!!pages.length &&
                 pages.map(({ label, path }) => (
                   <NavLink
                     key={`${label}-${path}`}
-                    className={({ isActive }) =>
-                      isActive ? "text-blue-600" : "text-black hover:text-blue-600"
-                    }
+                    // to={getPath(path)}
+                    className="text-black"
                     to={getPath(path)}
-                    onClick={() => setToggle(false)}
                   >
                     {label}
                   </NavLink>
@@ -50,16 +49,10 @@ const Navbar = () => {
           </div>
         )}
       </div>
-      <nav className="hidden sm:flex  gap-8 text-xl  ">
+      <nav className="hidden gap-8 text-xl sm:flex ">
         {!!pages.length &&
           pages.map(({ label, path }) => (
-            <NavLink
-              key={`${label}-${path}`}
-              to={getPath(path)}
-              className={({ isActive }) =>
-                isActive ? "text-blue-400" : "hover:text-blue-400"
-              }
-            >
+            <NavLink key={`${label}-${path}`} to={getPath(path)}>
               {label}
             </NavLink>
           ))}
