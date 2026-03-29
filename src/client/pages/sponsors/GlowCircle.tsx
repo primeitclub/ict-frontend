17fdaee6d871018c235c0d94ce4857e83eb43332
derikesh <rikeshsherpa1@gmail.com>
2026-03-29 18:38:55 +0545

    chore:build fix

--- src/client/pages/sponsors/GlowCircle.tsx
+++ src/client/pages/sponsors/GlowCircle.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-
-type position={
-    x:string , 
-    y:string
-}
-const GlowCircle = (props:position) => {
+type position = {
+  x: string;
+  y: string;
+};
+const GlowCircle = (props: position) => {
   return (
-    <div className={` ${props.x} ${props.y}  flex bg-[#02369E66] w-[282.06px] h-[283px] md:w-[666px] md:h-[666px] rounded-full absolute blur-[94px] md:blur-[200px]  -z-10`} ></div>
-  )
-}
+    <div
+      className={` ${props.x} ${props.y}  flex bg-[#02369E66] w-[282.06px] h-[283px] md:w-[666px] md:h-[666px] rounded-full absolute blur-[94px] md:blur-[200px]  -z-10`}
+    ></div>
+  );
+};
 
-export default GlowCircle
+export default GlowCircle;
