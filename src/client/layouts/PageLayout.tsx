e34d12b7346dff587552a863187f68b5821d9c6c
Nirjala Shakya <nirjala.shakya@amniltech.com>
2026-07-15 22:32:34 +0545

    feat: set browser favicon dynamically from the active edition's logo
    Falls back to the static /favicon.svg when an edition has no logo set.

--- src/client/layouts/PageLayout.tsx
+++ src/client/layouts/PageLayout.tsx
@@ -4,8 +4,11 @@ import { Footer } from "./footer/Footer";
 import Navbar from "./headers/Navbar";
 import VersionNavigate from "./version-navigate/VersionNavigate";
 import ScrollToTop from "../../global-wrappers/ScrollToTop";
+import { useFavicon } from "../hooks/use-favicon";
 
 export default function PageLayout() {
+  useFavicon();
+
   return (
     <div className="app-layout">
       <ScrollToTop />
